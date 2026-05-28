d427b4a6647323491a06aae7ff7be07cc11bdda7
NetAcad DEVASC <changeme@example.com>
2026-05-28 01:52:03 +0000

    Correccion punto en docker build

--- sample-app.sh
+++ sample-app.sh
@@ -4,6 +4,7 @@ echo "WORKDIR /app" >> Dockerfile
 echo "RUN echo '<h1>Evaluacion 2 Operativa en puerto 9999</h1>' > index.html" >> Dockerfile
 echo "EXPOSE 8080" >> Dockerfile
 echo "CMD [\"python\", \"-m\", \"http.server\", \"8080\"]" >> Dockerfile
-docker build -t sample-app
+
+docker build -t sample-app .
 docker rm -f sample-app-container || true
 docker run -t -d -p 9999:8080 --name sample-app-container sample-app
