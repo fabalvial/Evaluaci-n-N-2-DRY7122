ed8b405acf7595529a993bdb49d4db3c0b50b5a7
NetAcad DEVASC <changeme@example.com>
2026-05-28 01:49:19 +0000

    Se agrega compilacion de imagen Docker

--- sample-app.sh
+++ sample-app.sh
@@ -1,1 +1,9 @@
-docker run -t -d -p 9999:8080 sample-app
+echo "FROM python:3.9-alpine" > Dockerfile
+echo "RUN mkdir /app" >> Dockerfile
+echo "WORKDIR /app" >> Dockerfile
+echo "RUN echo '<h1>Evaluacion 2 Operativa en puerto 9999</h1>' > index.html" >> Dockerfile
+echo "EXPOSE 8080" >> Dockerfile
+echo "CMD [\"python\", \"-m\", \"http.server\", \"8080\"]" >> Dockerfile
+docker build -t sample-app
+docker rm -f sample-app-container || true
+docker run -t -d -p 9999:8080 --name sample-app-container sample-app
